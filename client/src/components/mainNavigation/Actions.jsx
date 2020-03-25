540591847a7092e6dabdd5adbc3f9573d1803064
Eric Lau <ericlaucmo@gmail.com>
2020-03-24 22:48:05 -0700

    Action icons obtained from AWS S3

--- client/src/components/mainNavigation/Actions.jsx
+++ client/src/components/mainNavigation/Actions.jsx
@@ -9,11 +9,17 @@ const Actions = ({
 }) => (
   <div className="navActions">
     <div className="actionItemsAccount" onClick={activateLoginModal}>
-      <img src="./assets/navTBox.png" alt="Image of a person" />
+      <img
+        src={`https://hrla35-fec-teamferrari-eric.s3.us-east-2.amazonaws.com/Images/Other/navTBox.png`}
+        alt="Image of a person"
+      />
       {userLoggedin ? <div>MY ACCOUNT</div> : <div>SIGN IN</div>}
     </div>
     <div className="actionItemsLocation" onClick={hideAllModals}>
-      <img src="./assets/navTBox.png" alt="Image of a location pin" />
+      <img
+        src={`https://hrla35-fec-teamferrari-eric.s3.us-east-2.amazonaws.com/Images/Other/navTBox.png`}
+        alt="Image of a location pin"
+      />
       <div>STORES</div>
     </div>
     <div
@@ -25,7 +31,10 @@ const Actions = ({
         }
       }}
     >
-      <img src="./assets/navTBox.png" alt="Image of a cart" />
+      <img
+        src={`https://hrla35-fec-teamferrari-eric.s3.us-east-2.amazonaws.com/Images/Other/navTBox.png`}
+        alt="Image of a cart"
+      />
       {cartQuantity > 0 ? (
         <div className="cartQuantity">{cartQuantity}</div>
       ) : null}
