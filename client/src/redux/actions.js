889043899602ac790e44a7f293ce3e1b62d3f00e
Eric Lau <ericlaucmo@gmail.com>
2020-05-05 23:34:49 -0700

    Modal toggling actions

---
+++ client/src/redux/actions.js
@@ -0,0 +1,39 @@
+export const showCart = () => ({
+  type: 'SHOW_CART'
+});
+
+export const hideCart = () => ({
+  type: 'HIDE_CART'
+});
+
+export const showMain = () => ({
+  type: 'SHOW_MAIN'
+});
+
+export const hideMain = () => ({
+  type: 'HIDE_MAIN'
+});
+
+export const showLogin = () => ({
+  type: 'SHOW_LOGIN'
+});
+
+export const hideLogin = () => ({
+  type: 'HIDE_LOGIN'
+});
+
+export const userLogin = () => ({
+  type: 'USER_LOGGED_IN'
+});
+
+export const userLogout = () => ({
+  type: 'USER_LOGGED_OUT'
+});
+
+export const showSearch = () => ({
+  type: 'SHOW_SEARCHES'
+});
+
+export const hideSearch = () => ({
+  type: 'HIDE_SEARCHES'
+});
